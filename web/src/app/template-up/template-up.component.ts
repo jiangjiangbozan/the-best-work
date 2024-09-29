1fe34fd630cc70d9b05f5c75d7d34ade1b7e1869
chenjunnian <3112037487@example.com>
2024-09-29 21:52:28 +0800

    刷新退出登陆的bug修改

--- web/src/app/template-up/template-up.component.ts
+++ web/src/app/template-up/template-up.component.ts
@@ -1,7 +1,7 @@
 
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import { User } from 'src/entity/user';
-
+import {UserService} from '../../service/user.service';
 
 @Component({
   selector: 'app-template-up',
@@ -12,14 +12,17 @@ export class TemplateUpComponent implements OnInit {
   @Output()
   beLogout = new EventEmitter<any>();
 
-   user = JSON.parse(sessionStorage.getItem('user')!) as User;
-  constructor() { }
+  user = JSON.parse(sessionStorage.getItem('user')!) as User;
+
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
   }
 
   onLogout(): void {
-    this.beLogout.emit();
+    this.userService.logout().subscribe(() => {
+      this.beLogout.emit();
+    })
   }
 
 }
